891e1d0fb83cbfda8d538135cb6c35551195faba
Michael Knight <MichaelGKnight87@gmail.com>
2016-12-04 09:56:51 +1000

    Severok: Object spawning

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+#!/bin/python
+
 import pyglet
+import copy	#Used to copy object prototypes to spawn active objects.
+import obj	#Object module	-Severok
+import ai	#AI	module	-Severok
+
+player_list = []	#List of objects in active play.
+enemy_list = []			#List of object prototypes in obj.py
+bullet_list = []
+misc_list = []
+
 
 window = pyglet.window.Window()
 
-img = pyglet.image.load('images/splash-mg.png')
-sprite = pyglet.sprite.Sprite(img, x = (window.width - img.width) // 2,
-    y = (window.height - img.height) // 2)
+obj.init_obj()
+
+
+#img = pyglet.image.load('images/splash-mg.png')
+#sprite = pyglet.sprite.Sprite(img, x = (window.width - img.width) // 2,
+#    y = (window.height - img.height) // 2)
+#objectlist.append sprite
+
+@window.event
+def on_mouse_motion(x,y,dx,dy):
+    pass
 
 @window.event
 def on_draw():
     window.clear()
-    sprite.draw()
+    for player in player_list:		#Render player sprite
+        player.sprite.draw()
+    for enemy in enemy_list:		#Render enemy sprites
+        enemy.sprite.draw()
+    for bullet in bullet_list:		#Render bullet sprites
+        bullet.sprite.draw()
+    for misc in misc_list:		#Render Misc sprites
+        misc.sprite.draw()
+
+
+#obj.spawn(misc_list,'misc',1,0,0)
+	#Initialisation
+obj.spawn(player_list,'player',0,(window.width)/2, (window.height)/2)
+obj.spawn(enemy_list,'enemy',0,100,100)
+obj.spawn(enemy_list,'enemy',1,200,200)
 
 pyglet.app.run()
---
+++ obj.py
@@ -0,0 +1,43 @@
+import copy
+import pyglet
+
+obj_enemy=[]	#Lists of object prototypes
+obj_player=[]		#Lists of objects in play in main.py  - Likely subject to clean up later.
+obj_bullet=[]
+obj_misc=[]
+
+obj_type={				#Define object_type dictionary. Used to parse objects into correct list for later referencing.
+    'enemy': obj_enemy,			#Object type Enemy.	(Add arbitary enemy times later)
+    'player': obj_player,			#Object type Player.	(Future potential for multiple player types or upgrades?)
+    'bullet': obj_bullet,			#Object type Bullet.	(Future potential for various projectiles?)
+    'misc': obj_misc}			#Object type Misc.	(Intended for graphical effects, Eg enemy dies spawn Explosion object)
+
+
+class objtype:
+    def __init__ (self, obj_id, health, img, size_x, behaviour):	#on object creation (Loading), object details loaded.
+        self.id = obj_id					#Todo: Check list on generation of conflicting IDs and throw error.
+        self.health = health				#Hits to remove || frames until timeout
+        self.img = pyglet.image.load(img)			#Load image for object
+        self.x = 0
+        self.y = 0
+        self.sprite = pyglet.sprite.Sprite(self.img,self.x,self.y)
+        self.ai = behaviour					#AI reference	- See ai.py
+
+def init_obj():
+    obfile = open('objlist','r');			#text file of tab seperated variables. Line = Object, tab = field.
+    for line in obfile:						#Id, Type, Health, Image location, sprite size, Behavior.
+        obparse = line.split()				#Parse read line into list for processing.
+        if (len(obparse) == 6):			#Line contains expected number of elements
+							#Populate relvent list as specified in line element[1] and object type dictionary.
+            new_object=objtype(obparse[0],obparse[2],obparse[3],obparse[4],obparse[5])
+            obj_type[obparse[1]].append(new_object)
+
+
+def spawn(obj_list,obj_proto,obj_id,x,y):	#References object from object list, copies object-prototype into active objects lists.
+    print(obj_type)
+    spawned = copy.copy(obj_type[obj_proto][obj_id])		#Make copy of object from prototype lists
+    spawned.x = x					#Set gameworld co-ord specifed
+    spawned.y = y
+    spawned.sprite = pyglet.sprite.Sprite(spawned.img,x,y)
+    obj_list.append(spawned)				#Append object to active lists so it is processed and rendered in game.
+	
