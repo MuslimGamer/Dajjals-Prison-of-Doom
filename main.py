43dfa1cbe6540fd2d743bfe73e9bb60c9fa1119e
ashes999 <ashes999@yahoo.com>
2016-12-02 21:54:43 -0500

    Code to create a colour sprite

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import pyglet
 
-window = pyglet.window.Window()
+COLOR_RED = (255, 0, 0, 255)
 
-img = pyglet.image.load('images/splash-mg.png')
-sprite = pyglet.sprite.Sprite(img, x = (window.width - img.width) // 2,
-    y = (window.height - img.height) // 2)
+class Main:
+    def run(self):
+        window = pyglet.window.Window()
+        
+        sprite = self.create_color(32, 32, COLOR_RED)
+        sprite.x = (window.width - sprite.width) // 2
+        sprite.y = (window.height - sprite.height) // 2
 
-@window.event
-def on_draw():
-    window.clear()
-    sprite.draw()
+        @window.event
+        def on_draw():
+            window.clear()
+            sprite.draw()
 
-pyglet.app.run()
+        pyglet.app.run()
+
+    # color_tuple is a four-colour tuple (R, G, B, A).
+    def create_color(self, width, height, color_tuple):
+        img = pyglet.image.SolidColorImagePattern(color_tuple).create_image(32, 32)
+        sprite = pyglet.sprite.Sprite(img)
+        return sprite
+
+Main().run()
+
+#img = pyglet.image.load('images/splash-mg.png')
+#sprite = pyglet.sprite.Sprite(img, x = (window.width - img.width) // 2,
+#    y = (window.height - img.height) // 2)
