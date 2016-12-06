420a05f7357cbb5d16b827cd9d76d3b565622e17
ashes999 <ashes999@yahoo.com>
2016-12-05 23:13:14 -0500

    MG and DG splash screens

--- main.py
+++ main.py
@@ -10,39 +10,30 @@ enemy_list = []			#List of object prototypes in obj.py
 bullet_list = []
 misc_list = []
 
-window = pyglet.window.Window()
-
-obj.load_prototype_data()
-#collision_map[window.width][window.height]
-
 # color_tuple is a four-colour tuple (R, G, B, A).
-def create_color(self, width, height, color_tuple):
+def create_color(width, height, color_tuple):
     img = pyglet.image.SolidColorImagePattern(color_tuple).create_image(32, 32)
     sprite = pyglet.sprite.Sprite(img)
     return sprite
 
-def create_image(self, image_filename):
+def create_image(image_filename):
     img = pyglet.image.load(image_filename)
     sprite = pyglet.sprite.Sprite(img)
     return sprite
 
-@window.event
-def on_mouse_motion(x,y,dx,dy):
-    # print('Mouse moved to {0}, {1}'.format(x, y))
-    # Note: pyglet uses a cartesian plane; positive y moves upward
-    pass
+def show_dg_splash():
+    splash_screen = obj.spawn('Misc', "DG Splash", 0, 0)
+    splash_screen.on_death = lambda: start_game()
+    misc_list.append(splash_screen)
 
-@window.event
-def on_draw():				#Kept seperate from processing callback, Frame rate not tied to simulation speed.
-    window.clear()
-    for player in player_list:		#Render player sprite
-        player.sprite.draw()
-    for enemy in enemy_list:		#Render enemy sprites
-        enemy.sprite.draw()
-    for bullet in bullet_list:		#Render bullet sprites
-        bullet.sprite.draw()
-    for misc in misc_list:		#Render Misc sprites
-        misc.sprite.draw()
+def start_game():
+    player = obj.spawn('Player', "Basic", window.width / 2, window.height / 2)
+    player_list.append(player)
+
+    e1 = obj.spawn('Enemy', "Basic", 100, 100)
+    e2 = obj.spawn('Enemy', "Coward", 200, 200)
+    enemy_list.append(e1)
+    enemy_list.append(e2)
 
 def frame_callback(dt):
     #Clear collision matrix
@@ -63,22 +54,37 @@ def frame_callback(dt):
 
     proc.ai(misc_list)              #Misc objects intended as cosmetic. No need to check collisions at this time.
     proc.update(misc_list)
-					
 
-def start_game():
-    player = obj.spawn('Player', "Basic", window.width / 2, window.height / 2)
-    player_list.append(player)
 
-    e1 = obj.spawn('Enemy', "Basic", 100, 100)
-    e2 = obj.spawn('Enemy', "Coward", 200, 200)
-    enemy_list.append(e1)
-    enemy_list.append(e2)
+window = pyglet.window.Window()
 
-dg_splash_screen = obj.spawn('Misc',"Splash", 0, 0)
-dg_splash_screen.on_death = lambda: start_game()
+@window.event
+def on_mouse_motion(x,y,dx,dy):
+    # print('Mouse moved to {0}, {1}'.format(x, y))
+    # Note: pyglet uses a cartesian plane; positive y moves upward
+    pass
+
+@window.event
+def on_draw():				#Kept seperate from processing callback, Frame rate not tied to simulation speed.
+    window.clear()
+    for player in player_list:		#Render player sprite
+        player.sprite.draw()
+    for enemy in enemy_list:		#Render enemy sprites
+        enemy.sprite.draw()
+    for bullet in bullet_list:		#Render bullet sprites
+        bullet.sprite.draw()
+    for misc in misc_list:		#Render Misc sprites
+        misc.sprite.draw()
+
+
+obj.load_prototype_data()
+#collision_map[window.width][window.height]
+
+dg_splash_screen = obj.spawn('Misc', "MG Splash", 0, 0)
+dg_splash_screen.on_death = lambda: show_dg_splash()
 misc_list.append(dg_splash_screen)
 
 pyglet.clock.schedule(frame_callback)
-pyglet.clock.schedule_interval(frame_callback, 1 / 30.0) # run at 30FPS
+pyglet.clock.schedule_interval(frame_callback, 1 / 30.0) # call frame_callback at 30FPS
 
 pyglet.app.run()
