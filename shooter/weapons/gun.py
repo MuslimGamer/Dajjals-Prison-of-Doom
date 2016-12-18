7804e7aab5568c7861e8a5690e485c694d1cd622
ashes999 <ashes999@yahoo.com>
2016-12-17 23:52:38 -0500

    Guns make noise.

--- shooter/weapons/gun.py
+++ shooter/weapons/gun.py
@@ -1,5 +1,7 @@
 import time
 
+import pyglet
+
 from shooter import config
 
 class Gun:
@@ -13,6 +15,7 @@ class Gun:
         self._cooldown_time_seconds = config.get("{0}_cooldown_seconds".format(gun_config_prefix))
         self.bullet_type = config.get("{0}_bullet_type".format(gun_config_prefix))
         self._last_shot = time.time()
+        self.__audio_file = "sounds/{0}.wav".format(gun_config_prefix)
 
     def reload(self):
         self._shots_left = 0 # triggers auto-reload
@@ -22,6 +25,7 @@ class Gun:
         if self._shots_left > 0 and time.time() - self._last_shot >= self._cooldown_time_seconds:
             self._shots_left -= 1
             self._last_shot = time.time()
+            pyglet.media.load(self.__audio_file, streaming=False).play()
             return True
         else:
             return False
