7b554d56ca3683de2feac29e652a2c239542f5d9
ashes999 <ashes999@yahoo.com>
2016-12-28 00:05:56 -0500

    Final, short tutorial (text only)

--- shooter/tutorials/story_and_controls.py
+++ shooter/tutorials/story_and_controls.py
@@ -4,7 +4,7 @@ import shooter.tutorials.base_tutorial
 
 class StoryAndControls(shooter.tutorials.base_tutorial.BaseTutorial):
     def __init__(self):
-        self.showing = "dajjal"
+        self.showing = "1"
         self.window = shooter.tutorials.speech_window.SpeechWindow()
         self.window.show("More Muslim meat for my armies! I'll chase you to the ends of the galaxy!", "dajjal")
         self.closed = False
@@ -14,10 +14,15 @@ class StoryAndControls(shooter.tutorials.base_tutorial.BaseTutorial):
 
     def update(self, key):
         if self.check_input_to_advance(key):
-            if self.showing == "dajjal":
-                self.showing = "hyperdrive"
-                self.window.show("Looks like the hyper-drive is offline. I'll need to rescue crew-members to get it operational again.", "captain")
-                self.window.show("Here come the Dajjal's war ships!", "captain")
+            if self.showing == "1":
+                self.showing = "2"
+                self.window.show("Looks like the hyper-drive is offline. I'll need to rescue enough escape pods to repair the ship so we can jump to safety.", "captain")
+            elif self.showing == "2":
+                self.showing = "3"
+                self.window.show("Use WASD to operate the thrusters, and the mouse to aim and shoot. R reloads the guns.", "captain")
+            elif self.showing == "3":
+                self.showing = "4"
+                self.window.show("Here come the Dajjal's war ships!", "captain")                
             else:
                self.window.close()
                self.closed = True
